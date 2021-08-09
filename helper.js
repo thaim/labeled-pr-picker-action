00fed912da17a274c36e1d839a2206e6c98d6012
thaim <thaim24@gmail.com>
2021-08-09 13:07:36 +0900

    add debug logging

--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core')
 
 module.exports.match_branch = async function (prdata, labels) {
-  console.info(`prdata: ${prdata}`);
+  console.info('prdata: ' + JSON.stringify(${prdata}));
   console.info(`labels: ${labels}`);
 
   return ['production'];
